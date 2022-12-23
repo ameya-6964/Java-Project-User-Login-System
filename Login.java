639d8e270710b2f331b6a2380c1b389bd9023d12
ameya-6964 <belvalkar7@gmail.com>
2022-12-23 13:40:59 +0530

    first commit

---
+++ Login.java
@@ -0,0 +1,105 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 661, 402);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("USER LOGIN ");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 35));
+		lblNewLabel.setBounds(221, 10, 228, 36);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("USER NAME");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_1.setBounds(29, 102, 154, 30);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("PASSWORD");
+		lblNewLabel_1_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_1_1.setBounds(29, 204, 154, 30);
+		frame.getContentPane().add(lblNewLabel_1_1);
+		
+		textField = new JTextField();
+		textField.setBounds(227, 102, 278, 29);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(227, 204, 278, 29);
+		frame.getContentPane().add(passwordField);
+		
+		JButton btnNewButton_1_1 = new JButton("LOGIN");
+		btnNewButton_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1_1.setBounds(154, 291, 110, 50);
+		frame.getContentPane().add(btnNewButton_1_1);
+		
+		JButton btnNewButton_1_1_1 = new JButton("RESET");
+		btnNewButton_1_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(null);
+				passwordField.setText(null);
+
+			}
+		});
+		btnNewButton_1_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		btnNewButton_1_1_1.setBounds(310, 291, 110, 50);
+		frame.getContentPane().add(btnNewButton_1_1_1);
+		
+		JButton btnNewButton_1_1_1_1 = new JButton("EXIT");
+		btnNewButton_1_1_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		btnNewButton_1_1_1_1.setBounds(472, 291, 110, 50);
+		frame.getContentPane().add(btnNewButton_1_1_1_1);
+	}
+}
